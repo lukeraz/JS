b12f711e9ed62d64655b0bd73dd0556a5dd21878
Lucas Nunes <freitaslucas@gmail.com>
2021-06-25 17:30:14 -0400

    condições

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 80.5
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if(vel > 80){ //condição simples
+    console.log(' voce passou da velocidade permitida, MULTADO!')
+}else{
+    console.log('Dirija sempre usando o cinto de segurança')
+}
+
+
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Estados Unidos'
+console.log(`Vivendo m ${pais}`)
+if(pais == 'Brasil'){
+    console.log('Brasileiro')
+}else{
+    console.log('Estrangeiro')
+}
