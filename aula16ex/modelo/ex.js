90d88b0f6a393f1ae9656ebc61cf0075501b24e2
Lucas Nunes <freitaslucas@gmail.com>
2021-07-29 17:41:42 -0400

    aulas

---
+++ aula16ex/modelo/ex.js
@@ -0,0 +1,9 @@
+let num = 0
+let vetor = [8, 7, 6, 5, 4, 3, 2, 1]
+
+if(vetor.indexOf(num) == -1){
+    console.log('O numero nao se encontra no vetor')
+}else{
+    console.log(`Este numero se encontra na ${vetor.indexOf(num)}° posição`)
+}
+
--- aula16ex/modelo/script.js
+++ aula16ex/modelo/script.js
@@ -1,25 +1,48 @@
-function adicionar(){
-    let num = document.getElementById('txtn')
-    let tab = document.getElementById('numtab')
-    if(num.value.length==0){
-        window.alert('ERRO! POR FAVOR INSIRA ALGUM NUMERO.')
-    
+let num = document.getElementById('txtn')
+let lista = document.getElementById('flista')
+let res = document.getElementById('res')
+let valores = []
+
+function isNumero(n){  //verificia se o valor digitado pelo usuario é um numero e esta entre 1 e 100
+    if(Number(n) >= 1 && Number(n) <= 100){   
+        return true
     }else{
-        let n = Number(num.value)
-        if(n > 100 || n < 1){
-            window.alert('ERRO! insira apenas numero de 1 a 100')
-        }else{
-            let vetor = [n]
-            for(let pos in vetor){
-                let item = document.createElement('option')
-                item.text=`O valor ${vetor[pos]} foi adicionado`
-                tab.appendChild(item)
-            }
-        }
+        return false
     }
 }
 
+function inLista(n, l){     //verifica se o valor digitado pelo usuario existe dentro da lista
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    }else{
+        return false
+    }
+}
+function adicionar(){       //verifica se ao adicionar o numero é valido e nao contem o mesmo valor dentro da lista
+    if(isNumero(num.value) && !inLista(num.value, valores)){
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text= (`Valor ${num.value} foi adicionado.`)
+        lista.appendChild(item)
+        
+    }else{
+        window.alert('ERRO! valor invalido ou  ja encontrado em lista')
+        
+    }
+    num.value='' // faz com que o numero digitado na barra de texto suma apos ser adicionado
+    num.focus() // faz com que voce nao precise clicar na barra de texto para digitar, apos ter adicionado um numero
+}
+
 function analizar(){
-   add = adicionar(vetor)
-    res.innerHTML=`Ao todo temos ${a}`
+    if(valores.length == 0){
+        window.alert('Adicione valores antes de analizar')
+    }else{
+        let tot= valores.length // faz com que ele leia quantos numero foram adicionados dentro do array, no caso "valores"
+        res.innerHTML=''
+        res.innerHTML+=`<p>Ao todo, temos ${tot} numeros cadastrados.</p>`
+    }
+}
+
+function excluir(){
+    
 }
